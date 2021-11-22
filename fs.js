89dd8f59486bfe3d0c4cbe034de53e02ff9dd535
Sathish Kumar <sathishweb27@gmail.com>
2021-11-22 13:21:18 +0530

    comit

--- fs.js
+++ fs.js
@@ -1,20 +1,33 @@
-import { writeFile } from 'fs';
-// fs.readFile('./cool.txt','utf-8', (err, data)=>{
+// import * as fs from 'fs';
+// fs.readFile('','utf-8', (err, data)=>{
 //      err? console.log(err) : console.log(data)})
 
 // write file
 
+// let dataToAppend = "Don't fall in a trap that come from prettiness "
 
+// // fs.writeFile('./newFile.txt', dataToWrite, (err) => console.log("Completed "))
 
-let dataToAppend = "Don't fall in a trap that come from prettiness "
+// // fs.appendFile('./newFile.txt', dataToAppend, err => console.log("appeded.. !!!") )
+// let date = new Date().toLocaleDateString('en-us')
+// console.log(date)
+// for(let i = 0 ; i< process.argv[2] || 1 ; i++){
+//     let dataToWrite = `<html><title>Back_up_${i} | ${date}</title><body><h2>Good Morning !!</h2><p>Today is ${date}</p></body></html>`
+//     writeFile(`./backups/test_${i}.html`, dataToWrite, (err) => console.log(`Back up ${i} Completed 👍`)) 
+// }
 
-// fs.writeFile('./newFile.txt', dataToWrite, (err) => console.log("Completed "))
+// import * as axios from 'axios';
 
-// fs.appendFile('./newFile.txt', dataToAppend, err => console.log("appeded.. !!!") )
-let date = new Date().toLocaleDateString('en-us')
-console.log(date)
-for(let i = 0 ; i< process.argv[2] || 1 ; i++){
-    let dataToWrite = `<html><title>Back_up_${i} | ${date}</title><body><h2>Good Morning !!</h2><p>Today is ${date}</p></body></html>`
-    writeFile(`./backups/test_${i}.html`, dataToWrite, (err) => console.log(`Back up ${i} Completed 👍`)) 
-}
+// axios.get('https://dev.to/aurelkurtula')
+//     .then((response) => {
+//         if(response.status === 200) {
+//         const html = response.data;
+//             const $ = cheerio.load(html); 
+//     }
+//     }, (error) => console.log(err) );
 
+import { parser } from 'html-metadata-parser';
+
+parser('https://www.youtube.com/watch?v=eSzNNYk7nVU').then(result=>{
+   console.log(JSON.stringify(result, null, 3));
+})
--- index.js
+++ index.js
@@ -1,36 +1,53 @@
-// const twice = (n) => n*2
-// console.log(process.argv[2])
-// const num = process.argv[2]
-// console.log(num)
-// console.log(twice(num))
 import { readFile, readFileSync, } from 'fs';
 import express from 'express';
+import { MongoClient } from 'mongodb';
+import dotenv from 'dotenv';
+
+dotenv.config()
 const app = express();
+const PORT = process.env.HOST_PORT
+app.use(express.json()) // make requests as json
 
-//method of rest api
+// mongo db config
+const MONGO_URL = process.env.DB_URL
 
-const PORT = 9000
+ // create db connection 
 
-// getData 
-let Movies = JSON.parse(readFileSync('./movies.json'))
-  let indexData = ''
+    async function createConnection(){
+    const client = new MongoClient(MONGO_URL);
+    await client.connect()
+    console.log("db connected")
+    return client
+    }
 
-  readFile('./test.html', 'utf-8', (err, data)=> err? console.log(err) : indexData = data)
+createConnection() 
+
+
+
+
+//  read html file 
+let indexData = ''
+readFile('./test.html', 'utf-8', (err, data)=> err? console.log(err) : indexData = data)
 
 app.get('/', (request, response)=>{
-    response.send(indexData)
+
+    response.send(indexData  || {message: "no data founded"})
 });
 // /movies
 //  with Queries
-const getQueryResults = (data, queries, queryNames, i) => data.filter(movie => `${movie[queryNames[i]]}` === queries[queryNames[i]].toUpperCase())
+const getQueryResults = (data, queries, queryNames, i) => {
+   let result =  data.filter(movie => `${movie[queryNames[i]]}` === queries[queryNames[i]].toUpperCase())
+
+   return result.length === 0 ? {message: "invalid query or data not found"} : result
+}
 
 const getGenreResult = (data, GenreQueries) =>{
         let ulti = []
+        GenreQueries = GenreQueries.split('-')
         data.forEach((movie)=> {
           for(let i = 0 ; i < movie.genre.length; i++ ){
             GenreQueries.forEach(query =>{
                  if(movie.genre[i].toUpperCase() === query.toUpperCase()){
-                    console.log(query)
                     ulti.push(movie)
                 }
             })
@@ -41,38 +58,45 @@ const getGenreResult = (data, GenreQueries) =>{
     
 }
 
-app.get('/movies', (request, response)=>{
-    let queries = request.query
-    queries.genre = queries.genre? queries.genre.split('-') : null
-    console.log(queries)
-    let queryNames = Object.keys(queries)
-    let queryResult = []
-    if(queryNames[0]==='genre'){
-        queryResult = getGenreResult(Movies, queries.genre)
-    }else{
-     queryResult = getQueryResults(Movies, queries, queryNames, 0)
-    }
-    
-    if (queryNames.length > 1){
-        for (let i = 1; i < queryNames.length; i++){
-            if(queryNames[i]==='genre' && queries.genre !== null) {
-                queryResult = getGenreResult(queryResult, queries.genre)
-            }
-            else if(queryNames[i]!=='genre') {
-                queryResult = getQueryResults(queryResult, queries, queryNames, i)
-            }
-        }
-    }
-    
-    let results = queryNames.length > 0 ? queryResult : Movies
-   
-    console.log(queries)
-    response.send(results)
+app.get('/movies', async (request, response)=>{
+    let filter = request.query
+    if(filter.Language){filter.Language = filter.Language.toUpperCase()}
+    if(filter.Rating) {filter.Rating = +filter.Rating } 
+    const client = await createConnection()
+    let results = await client
+    .db("Movies")
+    .collection("Movie DB")
+    .find(filter)
+    .toArray()
+    response.send(results || {message: "no data founded"})
 });
+
+
 // /movies/id
-app.get('/movies/:id', (request, response)=>{
-    console.log(request.params.id);
-    let filterResponse = Movies.find(movie => movie.id === request.params.id)
+app.get('/movies/:id', async (request, response)=>{
+    const client = await createConnection()
+    let filterResponse = await client
+    .db("Movies")
+    .collection("Movie DB")
+    .findOne({id: request.params.id})
     response.send(filterResponse || {message: "no data founded"})
 });
+
+
+// add movie data into DB
+
+app.post('/movies', async (request, response)=>{
+    
+    const data =  request.body
+    const client = await createConnection()
+    console.log(data)
+    let result = await client
+    .db("Movies")
+    .collection("Movie DB")
+    .insertMany(data)
+
+    response.send(result)
+});
+
+
 app.listen(PORT, ()=> console.log(" the started in port " + PORT))
