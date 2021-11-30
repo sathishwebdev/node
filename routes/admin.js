b7c7b96c0478d640ae01aa9f7b78be14599846ee
Sathish Kumar <sathishweb27@gmail.com>
2021-11-30 11:03:35 +0530

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getAdminByName} from '../helper.js';
+import {keyGenerator, createAdmin, getAdminByName} from '../helper.js';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
@@ -35,5 +35,34 @@ router
    
 })
 
+router
+.route('/')
+.post(async (req, res)=>{
+    // body will be none if do with query
+    const data = req.body;
+    const hashWord = await keyGenerator(data.password) 
+    //  check user avalaiblity
+    let checkUserAvailability = await getAdminByName(data.username)
+    
+    // check password strength 
+    let passwordStrength = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$_!%*#?&])[A-Za-z\d@_$!%*#?&]{6,}$/.test(`${data.password}`)
 
+    // check username
+    let usernameStrength = /^[A-Za-z\d_]{4,}$/.test(`${data.username}`)
+
+    // conditional statements
+    if(checkUserAvailability){
+        res.send({message : "Username Already Exist - Better Luck Next Time 😈"})
+    }else if (!passwordStrength){
+        res.send({message : `Password must be longer \n \t - Atleast 6 characters \n \t - Atleast one letter \n \t - Atleast one number \n \t - Atleast one special characters `})
+    }else if (!usernameStrength){
+        res.send({message : `Username should be min 3 characters - Only letters, numbers and _ can be accepteable`})
+    }
+    else{
+        data.password =  hashWord 
+        let db_responce = await createAdmin(data)
+        res.send(db_responce)
+    }
+    
+})
 export const adminRouter = router
